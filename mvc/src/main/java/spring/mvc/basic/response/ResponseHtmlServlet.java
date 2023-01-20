aebfaa274163bde788cc47f8f4832aa128e2c8f9
heaven <porori0703@ewhain.net>
2023-01-20 19:20:44 +0900

    text/html type response

---
+++ mvc/src/main/java/spring/mvc/basic/response/ResponseHtmlServlet.java
@@ -0,0 +1,27 @@
+package spring.mvc.basic.response;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/*
+타입이 text/html이면 자동으로 웹브라우저가 body의 html 데이터를 읽어서 웹브라우저로 렌더링
+ */
+@WebServlet(name="responseHtmlServlet",urlPatterns = "/response-html")
+public class ResponseHtmlServlet extends HttpServlet {
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html");
+        response.setCharacterEncoding("utf-8");
+        PrintWriter writer = response.getWriter();
+        writer.println("<html>");
+        writer.println("<body>");
+        writer.println("  <div>안녕?</div>");
+        writer.println("</body>");
+        writer.println("</html>");
+    }
+}
