e8ea44907dd55d7016da2860f6ced58463631abb
heaven <porori0703@ewhain.net>
2023-01-20 18:25:49 +0900

    servlet 사용해보기- 기본

---
+++ mvc/src/main/java/spring/mvc/basic/HelloServlet.java
@@ -0,0 +1,30 @@
+package spring.mvc.basic;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name="helloServlet",urlPatterns = "/hello")
+public class HelloServlet extends HttpServlet {
+
+    //서블릿이 호출되면 service 메서드가 호출된다
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        System.out.println("HelloServlet.service");
+        System.out.println("request = " + request);
+        System.out.println("response = " + response);
+
+        String test = request.getParameter("parameter");
+        System.out.println(test);
+
+        //response 헤더에 들어갈 내용
+        response.setContentType("text/plain");
+        response.setCharacterEncoding("utf-8");
+
+        // response body에 들어갈 내용
+        response.getWriter().write("hello"+test);
+    }
+}
