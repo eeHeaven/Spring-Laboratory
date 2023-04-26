d7830061321d313a09163ab46989ab1ae33ce07b
heaven <porori0703@ewhain.net>
2023-04-26 17:29:12 +0900

    @Aspect 활용한 AOP 기본 설정 및 기본 테스트

---
+++ aop/src/test/java/spring/aop/order/AopTest.java
@@ -0,0 +1,40 @@
+package spring.aop.order;
+
+import lombok.extern.slf4j.Slf4j;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.aop.support.AopUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+import spring.aop.order.aop.AspectV1;
+
+@Slf4j
+@SpringBootTest
+@Import(AspectV1.class)
+public class AopTest {
+
+    @Autowired
+    OrderService orderService;
+
+    @Autowired
+    OrderRepository orderRepository;
+
+    @Test
+    void aopInfo(){
+        log.info("isAopProxy, orderService = {}", AopUtils.isAopProxy(orderService));
+        log.info("isAopProxy, orderRepository = {}", AopUtils.isAopProxy(orderRepository));
+    }
+
+    @Test
+    void success(){
+        orderService.orderItem("test1");
+    }
+    @Test
+    void exception(){
+        Assertions.assertThatThrownBy(()-> orderService.orderItem("ex"))
+                .isInstanceOf(IllegalStateException.class);
+    }
+
+
+}
